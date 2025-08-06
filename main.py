2ceb4c484c1374c1a8ca80f16d422cfc9d54e20b
trailtony <still_running_uphill@protonmail.com>
2025-08-06 21:46:37 +0100

    Added categories input box and categories.json to store information

--- main.py
+++ main.py
@@ -8,15 +8,33 @@ import os
 
 st.set_page_config(page_title="Simple Finance App", page_icon="💰", layout="wide")
 
+category_file = "categories.json"
+
+if "categories" not in st.session_state:
+    st.session_state.categories = {
+        "Uncategorized": []
+    }
+    
+if os.path.exists(category_file):
+    with open(category_file, "r") as f:
+        try:
+            st.session_state.categories = json.load(f)
+        except RuntimeError as e:
+            print(str(e))
+        
+        
+def save_categories():
+    with open(category_file, "w") as f:
+        json.dump(st.session_state.categories, f)
+
 def load_transactions(file):
     try:
         df = pd.read_csv(file)
         df.columns = [col.strip() for col in df.columns]
         df["Amount"] = df["Amount"].str.replace(",", "").astype(float)
         df["Date"] = pd.to_datetime(df["Date"], format="%d %b %Y")
-        
-        st.write(df)
         return df
+    
     except Exception as e:
         st.error(f"Error processing file: {str(e)}")
         return None
@@ -36,6 +54,16 @@ def main():
             
             tab1, tab2 = st.tabs(["Expenses (Debits)", "Payments (Credits)"])
             with tab1:
+                new_category = st.text_input("New Category Name")
+                add_button = st.button("Add Category")
+                
+                if add_button and new_category:
+                    if new_category not in st.session_state.categories:
+                        st.session_state.categories[new_category] = []
+                        save_categories()
+                        st.success(f"Added a new category: {new_category}")
+                        st.rerun()
+                        
                 st.write(debits_df)
                 
             with tab2:
